f54102b6e7b7390040329649ffc75f2fcebf6fbe
Kasaksaxena <kasaksaxena6@gmail.com>
2024-07-07 22:21:12 +0530

    add cmplete and exit code

--- to-do list/gui.py
+++ to-do list/gui.py
@@ -7,10 +7,12 @@ add_button=sg.Button('ADD')
 list_box=sg.Listbox(values=functions.get_todos(), key='todos',
                     enable_events=True,size=[45,10])
 edit_button=sg.Button('EDIT')
+complete_button=sg.Button('Complete')
+exit_button=sg.Button('EXIT')
 
 window=sg.Window('My To-Do App',
                  layout=[[label,], [input_box,add_button],
-                         [list_box,edit_button]],
+                         [list_box,edit_button,complete_button],[exit_button]],
                  font=('Helvetica',20))
 
 while True:
@@ -39,7 +41,22 @@ while True:
             sg.popup("please select an item first",font=("Helvetica",20))  
       case "todos":
           window["todo"].update(value=values["todos"][0])    
+      case "Complete":
+        try:  
+          todo_to_complete= values["todos"][0]
+          todos=functions.get_todos()
+          todos.remove(todo_to_complete)
+         
+          functions.write_todos(todos)
+          window["todos"].update(values=todos)
+          window["todo"].update(value="")
           
+        except IndexError: 
+          sg.popup("please select an item first",font=("helvetica",20))  
+          
+      case "EXIT":
+           break   
+         
       case sg.WIN_CLOSED:  
           break     
   
