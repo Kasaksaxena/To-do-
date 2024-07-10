3a1509ca0b077c684df1bf0c93faba05361bac5e
Kasaksaxena <kasaksaxena6@gmail.com>
2024-07-10 18:14:43 +0530

    completing the todo items

--- to-do list/web.py
+++ to-do list/web.py
@@ -5,16 +5,24 @@ def add_todo():
     todo=st.session_state["new_todo"] +"\n"
     todos.append(todo)
     functions.write_todos(todos)
+    
 
 todos=functions.get_todos()
 st.title("MY TODO APP")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
-count=0
-for todo in todos:
-    st.checkbox(todo,key=count)
-    count+=1
+
+for index,todo in enumerate(todos):
+    checkbox=st.checkbox(todo,key=todo)
+    
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
     
     
 st.text_input(label="add todo",placeholder="Add new todo....",
               on_change=add_todo,key="new_todo")    
+
+#st.session_state
